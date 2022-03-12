6d5f3c80a1c02621f642938d5a010b279c225e8a
Fabricio Luis Perrone <fabricio_luis@outlook.com>
2022-03-11 16:08:05 -0800

    Atualização de código, boas práticas de programação

--- PedraPapelTesoura/jokenpo.java
+++ PedraPapelTesoura/jokenpo.java
@@ -6,56 +6,67 @@ public class jokenpo {
 
 	public static void main(String[] args) {
 		int jogador, computador;
+		int i;
+		final int papel = 0; // declaração constante
+		final int pedra = 1;
+		final int tesoura = 2;
+		
 		Scanner teclado = new Scanner(System.in);
-		System.out.println("-------Jokenpo-------");
+		System.out.println("-------Jokenpo Jogo Pedra Papel Tesoura-------");
 		System.out.println("");
+		System.out.println("0 papel");
 		System.out.println("1 pedra");
 		System.out.println("2 tesoura");
-		System.out.println("3 papel");
-		System.out.println("");
+		// System.out.println("");
 		System.out.println("Digite a opção de sua escolha: ");
 
 		// jogador
 		jogador = teclado.nextInt();
 		switch (jogador) {
-		case 1:
-			System.out.println("Jogador escolheu PEDRA ");
-			break;
-		case 2:
+		case papel:
 			System.out.println("Jogador escolheu PAPEL ");
 			break;
-		case 3:
+		case pedra:
+			System.out.println("Jogador escolheu PEDRA ");
+			break;
+		case tesoura:
 			System.out.println("Jogador escolheu TESOURA");
 			break;
 		default:
 			System.out.println("Opção inválida, selecione uma opção válida.");
-			break;
-
+			System.exit(0); // FINALIZA  APP
+			
 		}
+	
+		
+	
+			computador = (int) (Math.random() * 2);
+			System.out.println("");
+			System.out.println("Loading : " + " = "  + " sorteando " + computador);
+			System.out.println("");
 
-		// computador
-
-		computador = (int) (Math.random() * 3 + 1);// computador soteia n inteiro, x3 + 1, ou seja, se for 0 + 1 =1, se
-		teclado.close(); // for 1+1=2 e se for 2+1=3
-
+			teclado.close(); 
+	//LOGICA COMPUTADOR
 		switch (computador) {
-		case 1:
+		case pedra:
 			System.out.println("Computador escolheu PEDRA ");
 			break;
-		case 2:
-			System.out.println("Computador escolheu TESOURA ");
+		case papel:
+			System.out.println("Computador escolheu PAPEL ");
 			break;
-		case 3:
-			System.out.println("Computador escolheu PAPEL");
+			case tesoura:
+			System.out.println("Computador escolheu TESOURA");
 			break;
 		}
-
+//LOGICA SAIDAS
 		System.out.println("");
 		if (jogador == computador) {
 			System.out.println("Houve um empate");
 		} else {
-			if ((jogador == 1 && computador == 3) || (jogador == 2 && computador == 1)
-					|| (jogador == 3 && computador == 2)) {
+			boolean jogadorGanhou =  (jogador == papel && computador == pedra) || (jogador == tesoura && computador == papel)
+					|| (jogador == pedra && computador == tesoura);
+			if (jogadorGanhou)
+					{
 				System.out.println("Jogador ganhou, parabéns");
 			} else {
 				System.out.println("Computador ganhou!!");
